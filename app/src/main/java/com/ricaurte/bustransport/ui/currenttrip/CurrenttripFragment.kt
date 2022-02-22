b5f14bfec95b8b0410cf237adf90517a8476fd5b
Juan Sebastian Ricaurte Gonzalez <sebastian.ricaurte@udea.edu.com>
2022-02-21 21:06:33 -0500

    Funcionalidad bottom navegation

---
+++ app/src/main/java/com/ricaurte/bustransport/ui/currenttrip/CurrenttripFragment.kt
@@ -0,0 +1,32 @@
+package com.ricaurte.bustransport.ui.currenttrip
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ricaurte.bustransport.R
+
+class CurrenttripFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CurrenttripFragment()
+    }
+
+    private lateinit var viewModel: CurrenttripViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_currenttrip, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CurrenttripViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ricaurte/bustransport/ui/reserve/ReserveViewModel.kt
@@ -0,0 +1,7 @@
+package com.ricaurte.bustransport.ui.reserve
+
+import androidx.lifecycle.ViewModel
+
+class ReserveViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
