2fbd51696d718554ca18475128dd41d8b25207dc
Juan Sebastian Ricaurte Gonzalez <sebastian.ricaurte@udea.edu.com>
2022-04-01 20:00:45 -0500

    Mapas y correciones

--- app/src/main/java/com/ricaurte/bustransport/ui/approchlocation2/Approchlocation2Fragment.kt
+++ app/src/main/java/com/ricaurte/bustransport/ui/approchlocation2/Approchlocation2Fragment.kt
@@ -25,10 +25,19 @@ class Approchlocation2Fragment : Fragment() {
     private val callback = OnMapReadyCallback { googleMap ->
 
         val Santabarbara = LatLng(5.8735372, -75.5690521)
-        googleMap.addMarker(MarkerOptions().position(Santabarbara).title("Santa Bárbara").snippet("Terminal: Parque Principal de Santa Bárbara"))
+        googleMap.addMarker(MarkerOptions().position(Santabarbara).title("Santa Bárbara").snippet("Terminal: Parque Principal"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(Santabarbara))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Santabarbara,16F))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Santabarbara,14F))
 
+        val casablanca  = LatLng(5.878338, -75.5699316)
+        googleMap.addMarker(MarkerOptions().position(casablanca ).title("Estadero ").snippet("Estadero Casa Blanca"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(casablanca ))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(casablanca ,14F))
+
+        val santa_caldas   = LatLng(5.881941, -75.571442)
+        googleMap.addMarker(MarkerOptions().position(santa_caldas ).title("Kilometro 25 ").snippet("Via hacia Caldas"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(santa_caldas  ))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santa_caldas  ,14F))
     }
 
     override fun onCreateView(
